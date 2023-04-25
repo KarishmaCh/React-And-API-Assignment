423c9f7744389881726f90f875ebcfca5be6bed9
karishma chavan <89963571+KarishmaCh@users.noreply.github.com>
2023-04-25 23:14:46 +0530

    navbar ,sidebar and Add Matter functionality

---
+++ attorney-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from './Component/Navbar';
+import MatterForm from './Component/MatterForm';
+import SideBar from './Component/Sidebar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-md-2 mb-3">
+            {/* Left SideBar */}
+            <SideBar />
+          </div>
+          <div className="col-md-10 pt-3">
+            <Routes>
+            
+              <Route path="/MatterForm" element={<MatterForm />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ attorney-app/src/Component/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import MatterForm from './MatterForm';
+import '../App.css';
+
+interface SideBarProps { }
+
+const SideBar: React.FC<SideBarProps> = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="md" className="flex-column fixed-left">
+            <div className="sidebar-sticky pt-3">
+                <Navbar.Brand as={Link} to="/">
+                    Dashboard
+                </Navbar.Brand>
+                <Nav className="flex-column">
+
+                    <NavDropdown title="Matter">
+                        <NavDropdown.Item as={Link} to="/MatterForm">Add Matter</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/ViewMatter">View Matter</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </div>
+        </Navbar>
+    );
+};
+
+export default SideBar;
